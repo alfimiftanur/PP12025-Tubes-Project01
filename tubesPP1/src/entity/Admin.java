8c909f9d7ff39e3ce317f98ff1c772ab7d4f8bbc
aufaramdhn <ramadhanaufa3@gmail.com>
2025-05-28 21:57:25 +0700

    Admin constructor, setter, dan getter

--- tubesPP1/src/entity/Admin.java
+++ tubesPP1/src/entity/Admin.java
@@ -1,4 +1,39 @@
 package entity;
 
 public class Admin {
+    private String id;
+    private String nama;
+    private String password;
+
+    // Constructor
+    public Admin(String id, String nama, String password) {
+        this.id = id;
+        this.nama = nama;
+        this.password = password;
+    }
+
+    // Getter dan Setter
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
