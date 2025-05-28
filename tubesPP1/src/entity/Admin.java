07f52093e5fcc69ee34e7f9e438e4fc48b7a5352
alfimiftanur <alfinurhakim1234@gmail.com>
2025-05-28 21:20:08 +0700

    push barang.java dan tambah package

--- tubesPP1/src/entity/Admin.java
+++ tubesPP1/src/entity/Admin.java
@@ -1,6 +1,6 @@
-package tubes;
+package entity;
 
-public class test {
+public class Admin {
     public static void main(String[] args) {
         System.out.println("Test");
     }
---
+++ tubesPP1/src/entity/Barang.java
@@ -0,0 +1,68 @@
+package entity;
+
+public class Barang {
+    private String kode;
+    private String nama;
+    private String jenis;
+    private int stok;
+    private double harga;
+    Barang next;
+
+    // Constructor
+    public Barang(String kode, String nama, String jenis, int stok, double harga) {
+        this.kode = kode;
+        this.nama = nama;
+        this.jenis = jenis;
+        this.stok = stok;
+        this.harga = harga;
+    }
+
+    // Getter dan Setter
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public int getStok() {
+        return stok;
+    }
+
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public Barang getNext() {
+        return next;
+    }
+
+    public void setNext(Barang next) {
+        this.next = next;
+    }
+}
