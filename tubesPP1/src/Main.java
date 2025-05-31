bb8c4f38dfe32f364b6782508a09b70b79d724a1
alfimiftanur <alfinurhakim1234@gmail.com>
2025-06-01 00:01:16 +0700

    pull tubesPP1/src/Main.java

--- tubesPP1/src/Main.java
+++ tubesPP1/src/Main.java
@@ -1,36 +1,83 @@
 import java.util.Scanner;
+import service.Auth;
 import service.Gudang;
 
 public class Main {
     public static void main(String[] args) {
-        Gudang g = new Gudang();
+        Auth authService = new Auth();
         Scanner sc = new Scanner(System.in);
+        boolean isLoggedIn = false;
+
+        System.out.println("=== SISTEM LOGIN ===");
+        while (!isLoggedIn) {
+            System.out.println("1. Login");
+            System.out.println("2. Register");
+            System.out.println("3. Lihat Admin");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih: ");
+            String menu = sc.nextLine();
+
+            switch (menu) {
+                case "1":
+                    System.out.print("ID: ");
+                    String id = sc.nextLine();
+                    System.out.print("Password: ");
+                    String password = sc.nextLine();
+
+                    if (authService.login(id, password)) {
+                        System.out.println("Login berhasil!\n");
+                        isLoggedIn = true;
+                    } else {
+                        System.out.println("ID atau password salah.\n");
+                    }
+                    break;
+                case "2":
+                    authService.register();
+                    break;
+                case "3":
+                    authService.tampilkanAdmin();
+                    break;
+                case "0":
+                    System.out.println("Keluar dari program...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Pilihan tidak tersedia.\n");
+            }
+        }
+
+        Gudang g = new Gudang();
         int pilihan;
 
-        do{
-            System.out.println("Menu Pilihan");
+        do {
+            System.out.println("=== MENU GUDANG ===");
             System.out.println("1. Tambah Barang");
             System.out.println("2. Tampilkan Barang");
             System.out.println("3. Hapus Barang");
-            System.out.println("4. Cari Barang");
-            System.out.println("5. Update Barang");
             System.out.println("0. Keluar ");
             System.out.print("Pilihan: ");
             pilihan = sc.nextInt();
+            sc.nextLine();
 
-            switch (pilihan){
+            switch (pilihan) {
                 case 1:
                     g.tambahBarang();
                     break;
                 case 2:
                     g.tampilkanBarang();
                     break;
+                case 3:
+//                    g.hapusBarang();
+                    break;
                 case 0:
-                    System.out.println("Keluar");
+                    System.out.println("Keluar dari menu gudang...");
                     break;
                 default:
                     System.out.println("Pilihan Tidak Tersedia!");
             }
-        }while(pilihan != 0);
+            System.out.println();
+        } while (pilihan != 0);
+
+        sc.close();
     }
 }
